03339ff7da066575b47d68bd571b54eac926cd59
John Irle <irlejohn@gmail.com>
2022-02-19 14:39:56 -0600

    add tests for multipush and pop

--- tests/mapstack_spec.lua
+++ tests/mapstack_spec.lua
@@ -1,4 +1,4 @@
-local find_map = function(maps, lhs)
+local find_map = function(lhs)
   local maps = vim.api.nvim_get_keymap('n')
   for _, map in ipairs(maps) do
     if map.lhs == lhs then
@@ -7,7 +7,11 @@ local find_map = function(maps, lhs)
   end
 end
 
-describe("mapstack", function()
+describe("stackmap", function()
+  before_each(function()
+    require"stackmap"._clear()
+    pcall(vim.keymap.del, "n", "asdfasdf")
+  end)
   it("can be required", function()
     require("stackmap")
   end)
@@ -19,4 +23,44 @@ describe("mapstack", function()
     local found = find_map("asdfasdf")
     assert.are.same(rhs, found.rhs)
   end)
+
+  it("can push multiple mappings", function()
+    local rhs = "echo 'This is a test'"
+    require("stackmap").push("test1", "n", {["asdf_1"] = rhs .. "1", ["asdf_2"] = rhs .. "2"})
+
+    local found_1 = find_map("asdf_1")
+    assert.are.same(rhs .. "1", found_1.rhs)
+
+    local found_2 = find_map("asdf_2")
+    assert.are.same(rhs .. "2", found_2.rhs)
+  end)
+
+  it("can delete mappings after pop: no existing", function()
+    local rhs = "echo 'This is a test'"
+    require("stackmap").push("test1", "n", {asdfasdf = rhs})
+
+    local found = find_map("asdfasdf")
+    assert.are.same(rhs, found.rhs)
+
+    require("stackmap").pop("test1", "n")
+
+    local after_pop = find_map("asdfasdf")
+    assert.are.same(nil, after_pop)
+  end)
+
+  it("can delete mappings after pop: yes existing", function()
+    vim.keymap.set('n', 'asdfasdf', "echo 'OG MAPPING'")
+
+    local rhs = "echo 'This is a test'"
+    require("stackmap").push("test1", "n", {asdfasdf = rhs})
+
+    local found = find_map("asdfasdf")
+    assert.are.same(rhs, found.rhs)
+
+    require("stackmap").pop("test1", "n")
+
+    local after_pop = find_map("asdfasdf")
+    assert.are.same("echo 'OG MAPPING'", after_pop.rhs)
+  end)
+
 end)
